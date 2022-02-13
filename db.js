dd66715bd6745bc88fcb213f351af75ee0fdbed4
Vasily <ckpyt@inbox.ru>
2022-02-13 21:46:41 +0800

    change local to internet server (db postgre)

--- db.js
+++ db.js
@@ -1,13 +1,21 @@
-require('dotenv').config() 
-const {Sequelize} = require('sequelize')
+require('dotenv').config()
+const { Sequelize } = require('sequelize')
 
 module.exports = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        dialect: 'postgres',
-        host: process.env.DB_HOST,
-        port:process.env.DB_PORT
-    }
-)
+    "postgres://postgres:postgres@localhost/gql", {
+    dialect: 'postgres'
+}
+
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//         dialect: 'postgres',
+//         host: process.env.DB_HOST,
+//         port:process.env.DB_PORT
+//     }
+// )
+// const sequelize = new Sequelize("postgres://postgres:postgres@localhost/gql", {
+//   dialect: 'postgres'
+//   // anything else you want to pass
+// })
