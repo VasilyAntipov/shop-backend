df34ba0035967d7c2d50fc3ad6da180d543a9490
Vasily <ckpyt@inbox.ru>
2022-04-16 14:10:03 +0800

    update

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const { Sequelize } = require('sequelize')
 
 module.exports = new Sequelize(
-    `postgres://${process.env.DB_NAME}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}`, {
+    `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}`, {
     dialect: 'postgres'
 }
 )
